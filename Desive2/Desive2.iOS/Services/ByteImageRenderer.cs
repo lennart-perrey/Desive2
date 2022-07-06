10c0885cabc9983caeb1035ff182c7f56360580b
Lennart Perrey <lennart.perrey@gmail.com>
2022-07-06 11:47:35 +0200

    photo renderer

---
+++ Desive2/Desive2.iOS/Services/ByteImageRenderer.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+namespace Desive2.iOS.Services
+{
+    internal class ByteImageRenderer
+    {
+    }
+}
---
+++ Desive2/Desive2/Models/ByteImage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desive2.Models
+{
+    internal class ByteImage
+    {
+    }
+}
